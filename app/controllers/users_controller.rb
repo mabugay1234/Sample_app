50a3a4b270ce38c6c28b2fe5515c827d5c008ce1
mabugay1234 <mabugay1234@gmail.com>
2019-01-15 20:20:29 +0700

    Chap 10: Updating, showing, and deleting users
    Chap 8: Bassic login
    
    Chap 8: Bassic Login
    
    chap 9
    
    Chap: 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :find_user, only: %i(show destroy)
+  before_action :admin_user, only: %i(destroy)
+
+  def index
+    @users = User.page(params[:page]).per Settings.page_limit
+  end
+
   def new
     @user = User.new
   end
@@ -16,11 +25,30 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
-
     return if @user.present?
     flash[:error] = t ".not"
-    redirect_to :signup_path
+    redirect_to signup_path
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash[:danger] = t ".danger"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user&.destroy
+      flash[:success] = t ".success"
+    else
+      flash[:danger] = t ".danger"
+    end
+    redirect_to users_url
   end
 
   private
@@ -28,5 +56,25 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "ession.create.logged_danger"
+    redirect_to login_url
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def correct_user
+    find_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get "/login", to: "session#new"
     post "/login", to: "session#create"
     delete "/logout", to: "session#destroy"
+    post "/signup",  to: "users#create"
     resources :users
   end
 end
